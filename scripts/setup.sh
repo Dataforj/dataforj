b05058f74cd173f456a9d1bbd8331612f5da0be8
Ben Lawson <ben@bgl.io>
2021-04-29 23:30:36 -0400

    - setup script for virtualenv creation and dep loading - requirements.txt file to prep venv

---
+++ scripts/setup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -e
+
+# install venv and set requirements
+
+# upgrade pip
+python3 -m pip install --upgrade pip
+
+# install virtualenv
+pip3 install virtualenv
+
+# create dataforj virtualenv
+virtualenv venv
+
+# activate and install dependencies
+source venv/bin/activate
+
+# upgrade local pip
+/Users/benjaminlawson/Code/dataforj/venv/bin/python -m pip install --upgrade pip
+
+pip3 install -r requirements.txt
